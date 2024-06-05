7f834952ff539c01e20a3b177a2e6de2a7574e4a
Ruviel21 <calvinhobbeson.java@gmail.com>
2024-06-05 21:42:08 +0300

    3. Тестирование репозиториев [#504862]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -98,4 +98,17 @@ public class Vacancy {
     public void setFileId(int fileId) {
         this.fileId = fileId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vacancy vacancy = (Vacancy) o;
+        return id == vacancy.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
