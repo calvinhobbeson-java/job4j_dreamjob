2fbd18f7c0593d6963c6132fed526a38af3d0bf4
Ruviel21 <calvinhobbeson.java@gmail.com>
2024-06-21 20:19:06 +0300

    4. Многопоточность в базе данных [#504860]

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -30,6 +30,21 @@ public class UserController {
             model.addAttribute("message", "Пользователь с такой почтой уже существует");
             return "error/404";
         }
-        return "redirect/vacancies";
+        return "redirect:/vacancies";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user, Model model) {
+        var userOptional = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Почта или пароль введены неверно");
+            return "users/login";
+        }
+        return "redirect:/vacancies";
     }
 }
